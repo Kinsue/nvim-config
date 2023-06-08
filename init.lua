7584a7b05b154204c1e004ae4a398397ba4c9cfb
CSY <1362567009@qq.com>
2023-06-08 23:50:23 +0800

    add lazy nvim

--- init.lua
+++ init.lua
@@ -33,3 +33,18 @@ vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
 -- 可视行跳转按键 https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
 vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
 vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup()
