f17e0dd1a25932be9cb649680057d0a5d4562771
CSY <1362567009@qq.com>
2023-06-08 22:52:08 +0800

    create init.lua

---
+++ init.lua
@@ -0,0 +1,23 @@
+local set = vim.o
+
+set.rnu = true
+set.number = true
+-- set.clipboard = "unnamed"
+
+-- 在 copy 后高亮
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 500,
+		})
+	end,
+})
+
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+-- ESC 替换
+vim.keymap.set("i", "jj", "<esc>")
+
