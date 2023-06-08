b5a837e5d84bc1a42e03ecefe00b8a433617e446
CSY <1362567009@qq.com>
2023-06-08 23:38:25 +0800

    add some keymap

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local set = vim.o
 
 set.rnu = true
 set.number = true
--- set.clipboard = "unnamed"
+set.clipboard = "unnamed"
 
 -- 在 copy 后高亮
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
@@ -14,10 +14,22 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	end,
 })
 
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
+
+-- keybindings
+local opt = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
+
+
 -- ESC 替换
 vim.keymap.set("i", "jj", "<esc>")
 
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
+vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
+
+-- 可视行跳转按键 https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
