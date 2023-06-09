86a46ac5d67ef8198f25873dc8c265a0589446e1
CSY <1362567009@qq.com>
2023-06-09 16:15:32 +0800

    add nvim-lspconfig $ config lua and clangd lsp

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local set = vim.o
 set.rnu = true
 set.number = true
-set.clipboard = "unnamed"
+-- set.clipboard = "unnamed"
 
 -- 在 copy 后高亮
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
@@ -34,17 +34,19 @@ vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
+	-- ColorScheme
 	{
 		"RRethy/nvim-base16",
 		lazy = true,
@@ -52,25 +54,31 @@ require("lazy").setup({
 	-- Telescope
 	{
 		keys = {
-		      { "<leader>p", ":Telescope find_files<CR>", desc = "find files" },
-		      { "<leader>gr", ":Telescope live_grep<CR>", desc = "grep in files" },
-		      { "<leader>rs", ":Telescope resume<CR>", desc = "resume" },
-		      { "<leader>q", ":Telescope oldfiles<CR>", desc = "oldfiles" },
-		}, 
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                              , branch = '0.1.1',
+			{ "<leader>p",  ":Telescope find_files<CR>", desc = "find files" },
+			{ "<leader>gr", ":Telescope live_grep<CR>",  desc = "grep in files" },
+			{ "<leader>rs", ":Telescope resume<CR>",     desc = "resume" },
+			{ "<leader>q",  ":Telescope oldfiles<CR>",   desc = "oldfiles" },
+		},
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.1',
+		-- or                              , branch = '0.1.1',
 		dependencies = { 'nvim-lua/plenary.nvim' }
-	}, 
+	},
 	-- Mason
 	{
 		event = "VeryLazy",
 		"williamboman/mason.nvim",
 		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-		config = function()
-		    -- Mason Setup
-		    require("mason").setup()
-		end
 	},
+	-- lspconfig
+	{
+		event = "VeryLazy",
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim"
+		},
+	},
+
 	-- {
 	--   "folke/persistence.nvim",
 	--   event = "BufReadPre", -- this will only start session saving when an actual file was opened
@@ -86,3 +94,7 @@ vim.keymap.set("n", "<Leader>[", "<C-o>", opt)
 vim.keymap.set("n", "<Leader>]", "<C-i>", opt)
 
 
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("lspconfig").clangd.setup {}
+require("lspconfig").lua_ls.setup {}
