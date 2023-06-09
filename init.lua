ede45d216ac5b0265dad26e07469da6e215a171a
CSY <1362567009@qq.com>
2023-06-09 12:47:25 +0800

    add colorschme

--- init.lua
+++ init.lua
@@ -47,4 +47,10 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup()
+require("lazy").setup({
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+	},
+})
+vim.cmd.colorscheme("base16-tender")
