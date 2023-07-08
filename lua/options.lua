4ea16b30a30a12441b9b973b7cb4fe11b74807bc
Kinsue <1362567009@qq.com>
2023-07-08 11:29:16 +0800

    Edit some keymaps and modify init.lua to simplify the file

---
+++ lua/options.lua
@@ -0,0 +1,15 @@
+-- Tips: use `:h <option>` to figure out the meaning if needed 
+
+-- Tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+
+-- UI config
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = false
+vim.opt.showmode = false --不显示模式， 不需要“ -- INSERT --” 提示
+
+
